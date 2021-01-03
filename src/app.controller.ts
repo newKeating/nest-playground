ce84fc71fa0ec1e179e9b3def70c59cdec7c84e7
Peter Kim <kroits09@gmail.com>
2021-01-03 21:57:46 +0900

    Add e2e test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 
-@Controller('app')
+@Controller('')
 export class AppController {
   @Get()
   home() {
-    return 'Welcome to my Move API';
+    return 'Welcome to my Movie API';
   }
 }
